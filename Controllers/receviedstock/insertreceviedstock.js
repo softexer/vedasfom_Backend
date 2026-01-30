88cbe0d437ff1cc8b9dfeecd6b873d00db4f3b1a
talarivinod <tvndkumar@gmail.com>
2026-01-30 18:04:42 +0530

    updatedd
    Signed-off-by: talarivinod <tvndkumar@gmail.com>

--- Controllers/receviedstock/insertreceviedstock.js
+++ Controllers/receviedstock/insertreceviedstock.js
@@ -1,92 +1,69 @@
-
 var { addreceviedstockdatavalidations } = require('./validationsreceviedstock');
 var ReceviedStockData = require('../../app/Models/stock');
-// var Customer = require('../../app/Model/customers');
-var idb = require('../core/generateID')
-var addreceviedstockdata = async (req, res) => {
+var idb = require('../core/generateID');
+
+const addreceviedstockdata = async (req, res) => {
+  try {
+
+    // SAFE PARSE
+    let params;
     try {
-                    var params = JSON.parse(req.body.buydata);
-
-        var result = await addreceviedstockdatavalidations.validate(params);
-        if (result.error) {
-            console.log("error", result, result.error)
-            let message = result.error.details[0].message;
-            res.statusCode = 400;
-            return res.send(message)
-        }
-
-        var pid = idb.GenerateIDS(9);
-        var date = new Date().getTime();
-        var projectid = "vid" + "@" + pid + date;
-        var imageid = "vid" + pid + '@';
-            var ids = "stock" + '@' + idb.GenerateIDS(5)
-        if (req.files != null) {
-            var file = req.files.image;
-            var filename = req.files.image.name;
-            var filemvpath = './public/images/' + imageid + filename;
-            var filedbpath = '/images/' + imageid + filename;
-            file.mv(filemvpath, async (err) => {
-                if (err) {
-                    return callback({
-                        status: 200,
-                        data: {
-                            response: 0,
-                            message: "something went to wrong"
-                        }
-                    });
-                } else {
-                    try {
-                        var insertreceviedstockdata = await ReceviedStockData.insertMany([{
-                            LivestockID: ids,
-                            category: params.category,
-                            group: params.group,
-                            breederName: params.breederName,
-                            male: params.male,
-                            female: params.female,
-                            kids: params.kids,
-                            averageWeight: date,
-                            totalWeight: params.totalWeight,
-                            cost: params.cost,
-                            totalCost: params.totalCost,
-                            stand: params.stand,
-                            description: params.description,
-                            image: filedbpath
-                        }]);
-                        console.log(insertreceviedstockdata);
-                        return callback({
-                            status: 200,
-                            data: {
-                                response: 3,
-                                message: "Product inserted successfully"
-                            }
-                        });
-                    } catch (insertError) {
-                        console.error(insertError);
-                        return callback({
-                            status: 200,
-                            data: {
-                                response: 0,
-                                message: "Product inserted Failure"
-                            }
-                        });
-                    }
-                }
-            });
-        } else {
-            return callback({
-                status: 200,
-                data: {
-                    response: 0,
-                    message: "please upload image"
-                }
-            })
-        }
-
-    } catch (error) {
-        console.log(error)
+      params = typeof req.body.buydata === "string"
+        ? JSON.parse(req.body.buydata)
+        : req.body.buydata;
+    } catch {
+      return res.status(400).json({ message: "Invalid buydata JSON" });
     }
 
-}
-module.exports = {
-    addreceviedstockdata
-}
+    // VALIDATION
+    const { error } = addreceviedstockdatavalidations.validate(params);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
+    // IMAGE CHECK
+    if (!req.files || !req.files.image) {
+      return res.status(400).json({ message: "Image is required" });
+    }
+
+    const file = req.files.image;
+    const imageid = "vid@" + idb.GenerateIDS(9);
+    const filename = imageid + file.name;
+    const filemvpath = `./public/images/${filename}`;
+    const filedbpath = `/images/${filename}`;
+
+    // MOVE FILE (PROMISE SAFE)
+    await new Promise((resolve, reject) => {
+      file.mv(filemvpath, err => err ? reject(err) : resolve());
+    });
+
+    // INSERT DATA
+    await ReceviedStockData.create({
+      LivestockID: "stock@" + idb.GenerateIDS(5),
+      category: params.category,
+      group: params.group,
+      breederName: params.breederName,
+      male: params.male,
+      female: params.female,
+      kids: params.kids,
+      averageWeight: params.averageWeight,
+      totalWeight: params.totalWeight,
+      cost: params.cost,
+      totalCost: params.totalCost,
+      stand: params.stand,
+      description: params.description,
+      image: filedbpath
+    });
+
+    return res.status(200).json({
+      response: 3,
+      message: "Product inserted successfully"
+    });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { addreceviedstockdata };
