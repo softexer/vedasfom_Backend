abf027c68b70ea7afffb88e3791dc0ad0c5ad43a
talarivinod <tvndkumar@gmail.com>
2026-01-31 10:14:20 +0530

    updatedd
    Signed-off-by: talarivinod <tvndkumar@gmail.com>

--- routes/receviedstock.js
+++ routes/receviedstock.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var ReceviedStock = require('../Controllers/receviedstock/receviedstock');
-
+var ReceviedStockData = require('../app/Models/stock');
 var fileupload = require('express-fileupload');
 router.use(fileupload({ limits: { fileSize: 50 * 1024 * 1024 } }));
 router.post("/addreceviedstock",(req,res)=>{
@@ -15,6 +15,43 @@ router.post("/fetchreceviedstock",(req,res,next)=>{
 })
 router.delete("/deletereceviedstock",(req,res,next)=>{
     ReceviedStock.deletereceviedstock(req,res)
+})
+router.get("/fetchstocktest",(req,res)=>{
+    ReceviedStockData.aggregate([
+  {
+    $group: {
+      _id: {
+        category: "$category",
+        group: "$group"
+      },
+      male: { $sum: { $toInt: "$male" } },
+      female: { $sum: { $toInt: "$female" } },
+      averageWeight: { $avg: { $toDouble: "$averageWeight" } }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      category: "$_id.category",
+      group: "$_id.group",
+      male: { $toString: "$male" },
+      female: { $toString: "$female" },
+      averageWeight: {
+        $toString: { $round: ["$averageWeight", 2] }
+      }
+    }
+  }
+])
+
+    .then(result => {
+
+         res.json({ response: 3, message: "Total stock data fetch successfully",TotalStcok:result})
+
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        res.status(500).json({ error: err.message });
+    });
 })
 module.exports = router;
 
